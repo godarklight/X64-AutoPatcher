af7b2981626ef214a5034d3a4ade4ad221d9a63d
Christopher Andrews <godarklight@gmail.com>
2014-12-09 14:22:44 +1000

    Add warning/disclaimer for users.

--- X64-AutoPatcher/MainClass.cs
+++ X64-AutoPatcher/MainClass.cs
@@ -30,16 +30,56 @@ namespace X64AutoPatcher
             {
                 kspPathDir = Directory.GetParent(ourPath).FullName;
             }
-            Console.WriteLine(kspPathDir);
-            Console.WriteLine(gameDataDir);
+            Console.WriteLine("KSP Path: " + kspPathDir);
+            Console.WriteLine("GameData Path: " + gameDataDir);
             if (!CecilReflector.LoadCecil(kspPathDir))
             {
                 Console.WriteLine("Mono.Cecil not found!");
                 AskToExit();
                 return;
             }
+            Console.WriteLine("==========");
+            if (!UserAcceptedMessage())
+            {
+                Console.WriteLine("You need to accept by typing 'yes'.");
+                AskToExit();
+                return;
+            }
+            Console.WriteLine("==========");
             AssemblyPatcher.PatchFiles(gameDataDir);
             AskToExit();
+        }
+
+        private static bool UserAcceptedMessage()
+        {
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.Write("WARNING: The 64 bit windows version of KSP is ");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.Write("unstable");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("!");
+            Console.ResetColor();
+            Console.Write("It is ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("highly recommended ");
+            Console.ResetColor();
+            Console.Write("to first try ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("-force-opengl");
+            Console.ResetColor();
+            Console.WriteLine(" with the 32bit version of KSP before attempting to use the 64bit version.");
+            Console.WriteLine();
+            Console.WriteLine("You may only use this program with the following restrictions:");
+            Console.WriteLine("1. You will not redistribute the edited binaries.");
+            Console.WriteLine("2. You will not ask the official modders for support.");
+            Console.WriteLine();
+            Console.ResetColor();
+            Console.Write("If you accept these conditions, type ");
+            Console.ForegroundColor = ConsoleColor.DarkGreen;
+            Console.Write("yes");
+            Console.ResetColor();
+            Console.WriteLine(" and then press enter");
+            return (Console.ReadLine().ToLower() == "yes");
         }
 
         private static void AskToExit()
